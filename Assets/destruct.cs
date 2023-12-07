2b5604806d1d4f9ba4192ec193847e2f12abba46
bcgkky <bcgkky1@gmail.com>
2023-12-07 13:29:34 +0300

    last
    ...

---
+++ Assets/destruct.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destruct : MonoBehaviour
+{   
+    public GameObject patlama;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            
+            if (Physics.Raycast(ray,out hit,Mathf.Infinity))
+            {
+                if (hit.transform.gameObject.CompareTag("lavuk"))
+                {
+                    Destroy(hit.transform.gameObject.GetComponent<BoxCollider>());
+                    GameObject efekt = Instantiate(patlama, hit.transform.position, hit.transform.rotation);
+
+                    Transform[] bebeler = hit.transform.gameObject.GetComponentsInChildren<Transform>();
+
+                    foreach (var bebelaklar in bebeler)
+                    {
+                        bebelaklar.gameObject.AddComponent<Rigidbody>();
+                    }
+                    Destroy(efekt, 1.5f);
+                }
+            }  
+        }
+    }
+}
